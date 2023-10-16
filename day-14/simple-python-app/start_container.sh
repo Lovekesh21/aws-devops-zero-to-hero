a9125f0cfef326003c283e05704f9ae757afc602
Lovekesh21 <110743411+Lovekesh21@users.noreply.github.com>
2023-10-16 12:14:36 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
 
-# Full path to the Docker executable (replace with the actual path)
-DOCKER_EXECUTABLE="/usr/bin/docker"
+
 
 # Pull the Docker image from Docker Hub
-$DOCKER_EXECUTABLE pull nginx:latest
+echo
 
 # Run the Docker image as a container
-$DOCKER_EXECUTABLE run -d --name first-conatiner nginx:latest
+echo
