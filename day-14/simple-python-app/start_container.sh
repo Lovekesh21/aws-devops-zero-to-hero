e47e9fb9ca175fbfffb2e5a45d99f5a045c5dac0
Lovekesh21 <110743411+Lovekesh21@users.noreply.github.com>
2023-10-17 12:19:54 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 
-
-
 # Pull the Docker image from Docker Hub
-echo
+docker pull lovishh/ec2-test-repo:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 6000:6000 lovishh/ec2-test-repo:latest
