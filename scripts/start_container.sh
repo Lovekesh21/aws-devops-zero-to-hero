e5ed2affd5820e3cb81d2a1a930f5ce88ff943b6
Lovekesh21 <110743411+Lovekesh21@users.noreply.github.com>
2023-10-17 12:08:54 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,9 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull lovishh/ec2-test-repo:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
-
-
+docker run -d -p 5000:5000 lovishh/ec2-test-repo:latest
