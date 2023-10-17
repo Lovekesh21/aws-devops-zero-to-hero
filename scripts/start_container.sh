656df6d43ee8e51b335c4070faed2055d959683a
Lovekesh21 <110743411+Lovekesh21@users.noreply.github.com>
2023-10-17 14:45:17 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e
 
+# Check if Docker is installed
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Installing Docker..."
+    
+    # Install Docker (Assuming you are using a Debian-based system)
+    sudo apt-get update
+    sudo apt-get install -y docker.io
+
+    # Add the user to the docker group (optional)
+    sudo usermod -aG docker $USER
+    
+    echo "Docker installation complete."
+fi
+
 # Pull the Docker image from Docker Hub
 docker pull lovishh/ec2-test-repo:latest
 
